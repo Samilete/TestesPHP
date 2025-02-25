cb47d1f02bd35d8e2253def6e75d1300c677f1b8
Samilly Ellen de Castro <samilly.castro@escolar.ifrn.edu.br>
2025-02-25 10:57:46 -0300

    Update insertcrud.php

--- insertcrud.php
+++ insertcrud.php
@@ -1,22 +1,36 @@
-// TEM O ADICIONAR DO DONO 
-
 <?php
+// TEM O ADICIONAR DO DONO 
 try {
-	$con = new PDO('pgsql:host=localhost;port=5432;dbname=20221214010024',
-	'postgres', 'pabd');
-	
-	if ($con) {
-		$nome = $_POST['nome'];
-		$valor  = $_POST['valor'];
-        $id  = $_POST['id'];
-        $quantidade  = $_POST['quantidade'];
-		
-		
-		$comando3 = "INSERT INTO itens VALUES ('$id', '$valor','$quantidade', '$nome')";
-		
-		$con->exec($comando3);
+	session_start();
+	include_once('conect.php');
+
+	if((!isset($_SESSION['nome']) == true ) and (!isset($_SESSION['senha']) == true))
+	{
+		unset($_SESSION['nome']);
+		unset($_SESSION['senha']);
+		header('location: home.php');
 	}
-} catch (PDOException $e) {
-	echo 'DEU ERRADO!!!' . $e;
+
+	$sql = "SELECT * FROM catalogo ORDER BY id_produto DESC";
+
+	$result = $con->query($sql);
+
+	print_r($result);
+
+		$con = new PDO('pgsql:host=localhost;port=5432;dbname=20221214010023',
+		'postgres', 'cams05');
+		
+		if ($con) {
+			$id_produto = $_POST['id_produto'];
+			$nome_produto  = $_POST['nome_produto'];
+			$valor_produto  = $_POST['valor_produto'];
+			$tempo_producao  = $_POST['tempo_producao'];
+			
+			
+			$comando3 = "INSERT INTO catalogo VALUES ('$id_produto', '$nome_produto','$valor_produto', '$tempo_producao')";
+			
+			$con->exec($comando3);
+		}
+	} catch (PDOException $e) {
+		echo 'DEU ERRADO!!!' . $e;
 }
-?>
